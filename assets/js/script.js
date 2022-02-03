94130230c15394615d2a61ce40f0bdf44dcd671b
Abbush <glgberat@yahoo.com>
2022-02-02 21:16:23 -0500

    data works

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,18 +14,42 @@ var getApi= function(event) {
   searchword = nameInputEl.value.trim();
   var radioValue = $("input[name='book']:checked").val();
 console.log(radioValue);
-var requestUrl2= "https://openlibrary.org/search/"+radioValue+"s.json?q="+searchword;
+
+if(radioValue=="title" || radioValue=="author" || radioValue=="publisher") {
+var Gapivalue="in"+radioValue;
+console.log(Gapivalue);
+
+}
+
+var requestUrl2= "https://openlibrary.org/"+radioValue+"/"+searchword+".json";
 
   //https://www.googleapis.com/books/v1/volumes?q="+radiovalue+"isbn:keyes&key="+GBooksApiID;
 
   var requestUrl = "https://api.nytimes.com/svc/books/v3/reviews?"+radioValue+"="+searchword+"&api-key="+NYTapiID;
-  var requestUrl3="https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyBHindfXjGQKCb_YXtPUsp1Ectnqpf0PmA";
+  var requestUrl3="https://www.googleapis.com/books/v1/volumes?q="+searchword+"+"+Gapivalue+":keyes&key="+GBooksApiID;
+
+  console.log(requestUrl3);
   
+ // https://www.googleapis.com/books/v1/volumes?q=search+terms
+ //https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+
+/*
+intitle: Returns results where the text following this keyword is found in the title.
+inauthor: Returns results where the text following this keyword is found in the author.
+inpublisher: Returns results where the text following this keyword is found in the publisher.
+subject: Returns results where the text following this keyword is listed in the category list of the volume.
+isbn: Returns results where the text following this keyword is the ISBN number.
+lccn: Returns results where the text following this keyword is the Library of Congress Control Number.
+oclc: Returns results where the text following this keyword is the Online Computer Library Center number.
+
+
+
+
+*/
 
 
   Promise.all([
     fetch(requestUrl),
-    fetch(requestUrl2),
     fetch(requestUrl3)
   ]).then(function (responses) {
     // Get a JSON object from each of the responses
