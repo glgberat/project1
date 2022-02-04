27933abd47279c552099af6045d0ea63276d061d
Abbush <glgberat@yahoo.com>
2022-02-04 01:21:05 -0500

    bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ aEL.href="https://www.amazon.com/dp/"+data[i].isbn_10[0];
 
 var imgEL=document.createElement("img");
 imgEL.className="card-img-top";
-imgEL.src="https://storage.googleapis.com/du-prd/books/images/"+data[i].isbn_13[0]+".jpg";
+imgEL.src=data[1].items[0].volumeInfo.imageLinks.thumbnail;
 aEL.appendChild(imgEL);
 cardsEl.appendChild(aEL);
 
@@ -38,33 +38,45 @@ searchResEl.appendChild(cardsEl);
 
 
 
-function NYTBest(data) 
+function NYTBest() 
 {
 
+  nytbestlisturl="https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?&api-key=C64Wxwb4zFKo1Au8JiTYdjhCMzmt6ev0";
+  fetch(nytbestlisturl).then(function(response) {
+      
+    response.json().then(function(data) {
+      for(i=0;i<5;i++) {
+        var nycardEl=document.createElement("div");
+        nycardEl.className="card";
+        NYTEl.appendChild(nycardEl);
+        var aEL=document.createElement("a");
+        aEL.href=data.results.books[i].amazon_product_url;
+        var imgEL=document.createElement("img");
+        imgEL.className="card-img-top";
+        imgEL.src=data.results.books[i].book_image;
+        
+        aEL.appendChild(imgEL);
+        nycardEl.appendChild(aEL);
+        
+        
+        var nycardfooterEl=document.createElement("div");
+        nycardfooterEl.className="card-footer";
+        var footersmall=document.createElement("small");
+        footersmall.className="text-muted";
+        footersmall.innerHTML="Rank: #"+(i+1);
+        nycardfooterEl.appendChild(footersmall);
+        nycardEl.appendChild(nycardfooterEl);
+        
+        }
+          
+      
+      
+
+          
+    });
+  });
   
-for(i=0;i<5;i++) {
-var nycardEl=document.createElement("div");
-nycardEl.className="card";
-NYTEl.appendChild(nycardEl);
-var aEL=document.createElement("a");
-aEL.href=data[2].results.books[i].amazon_product_url;
-var imgEL=document.createElement("img");
-imgEL.className="card-img-top";
-imgEL.src=data[2].results.books[i].book_image;
 
-aEL.appendChild(imgEL);
-nycardEl.appendChild(aEL);
-
-
-var nycardfooterEl=document.createElement("div");
-nycardfooterEl.className="card-footer";
-var footersmall=document.createElement("small");
-footersmall.className="text-muted";
-footersmall.innerHTML="Rank: #"+(i+1);
-nycardfooterEl.appendChild(footersmall);
-nycardEl.appendChild(nycardfooterEl);
-
-}
 
 }
 
@@ -79,30 +91,30 @@ console.log(radioValue);
 
 if(radioValue=="title")
 {
-  requestUrl2="http://openlibrary.org/search.json?title="+searchword; 
+  requestUrl1="http://openlibrary.org/search.json?title="+searchword; 
 }// to search title
   else if(radioValue=="subject")
   {
-requestUrl2="http://openlibrary.org/subjects/"+searchword+".json"; // to search genre or subject
+requestUrl1="http://openlibrary.org/subjects/"+searchword+".json"; // to search genre or subject
 
   }
 else if(radioValue=="author")
 {
-  requestUrl2="https://openlibrary.org/search/authors.json?q="+searchword;
+  requestUrl1="https://openlibrary.org/search/authors.json?q="+searchword;
 
 }
 
   else 
   {
-    requestUrl2="https://openlibrary.org/"+radioValue+"/"+searchword+".json"; // to search authors and isbn#
+    requestUrl1="https://openlibrary.org/"+radioValue+"/"+searchword+".json"; // to search authors and isbn#
 
   }
 
 
-  console.log(requestUrl2);
+  console.log(requestUrl1);
 
 
-nytbestlisturl="https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?&api-key=C64Wxwb4zFKo1Au8JiTYdjhCMzmt6ev0";
+
 
 
 
@@ -110,7 +122,7 @@ nytbestlisturl="https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.jso
 
   var requestUrl = "https://api.nytimes.com/svc/books/v3/reviews?"+radioValue+"="+searchword+"&api-key="+NYTapiID;
 
- 
+ var requestUrl2 ="https://www.googleapis.com/books/v1/volumes?q="+searchword+"isbn&key="+GBooksApiID;
 
 
 
@@ -121,8 +133,8 @@ nytbestlisturl="https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.jso
 
 
   Promise.all([
+    fetch(requestUrl1),
     fetch(requestUrl2),
-    fetch(requestUrl),
     fetch(nytbestlisturl)
   ]).then(function (responses) {
     // Get a JSON object from each of the responses
@@ -132,9 +144,7 @@ nytbestlisturl="https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.jso
   }).then(function (data) {
     // Log the data to the console
     // You would do something with both sets of data here
-    console.log(data);
-    console.log(data[0].number_of_pages);
-    console.log(data[1].num_results);
+    
 
     NYTBest(data);
     BooksbyISBN(data);
@@ -152,9 +162,13 @@ nytbestlisturl="https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.jso
 
 
 
-
+NYTBest();
 
 userFormEl.addEventListener("submit", getApi);
+
+
+
+
 
 
 
