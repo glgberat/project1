001eefe96ec5f16f55b1ba7cace95bc06f276bfb
Gravity <tevoned@gmail.com>
2022-02-07 19:54:11 -0500

    hero image fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-var data1;
 var userFormEl = document.querySelector("#user-form");
 var searchButton = document.getElementById('searchbtn');
 var NYTEl = document.getElementById('nyt');
@@ -12,61 +11,69 @@ var datafromNYT;
 var searchword;
 var requestUrl2;
 
-function BooksbyISBN2(data){
-
-var cardsEl=document.createElement("div");
-cardsEl.className="card h-100";
 
 
-var aEL=document.createElement("a");
-aEL.href="https://www.amazon.com/dp/"+data[0].isbn_10[0];
-aEL.target="_blank";
-
-var imgEL=document.createElement("img");
-imgEL.className="card-img-top";
-imgEL.src="https://covers.openlibrary.org/b/isbn/"+data[0].isbn_10[0]+"-L.jpg";
-
-var cardbodyEl=document.createElement("div");
-cardbodyEl.className="card-body";
-cardsEl.appendChild(cardbodyEl);
-
-var h5El=document.createElement("h5");
-h5El.className="card-title";
-cardbodyEl.appendChild(h5El);
-
-var pEL=document.createElement("p");
-pel.className="card-text";
-cardbodyEl.appendChild(pEL);
 
 
-aEL.appendChild(imgEL);
-cardsEl.appendChild(aEL);
 
-searchResEl.appendChild(cardsEl);
-
-}
-
-function BooksbyISBN(data)
+function BooksbyISBN(data) //google books api
 {
+  searchResEl.innerHTML="";
 
-var cardsEl=document.createElement("div");
+  for(i=0;i<data[0].items.length;i++)
+  {
+    var cardsEl=document.createElement("div");
 cardsEl.className="card";
 
 
 var aEL=document.createElement("a");
-aEL.href="https://www.amazon.com/dp/"+data[0].isbn_10[0];
+aEL.href=data[0].items[i].volumeInfo.canonicalVolumeLink;
 aEL.target="_blank";
 
 var imgEL=document.createElement("img");
 imgEL.className="card-img-top";
-imgEL.src=data[1].items[0].volumeInfo.imageLinks.thumbnail;
-
+imgEL.src=data[0].items[i].volumeInfo.imageLinks.thumbnail;
 aEL.appendChild(imgEL);
 cardsEl.appendChild(aEL);
 
 searchResEl.appendChild(cardsEl);
 
 }
+
+}
+
+
+
+function BooksbySubject(data)
+{
+  searchResEl.innerHTML="";
+
+  for(i=0;i<data[0].items.length;i++)
+  {
+    var cardsEl=document.createElement("div");
+cardsEl.className="card";
+
+
+var aEL=document.createElement("a");
+aEL.href=data[0].items[i].volumeInfo.canonicalVolumeLink;
+aEL.target="_blank";
+
+var imgEL=document.createElement("img");
+imgEL.className="card-img-top";
+imgEL.src=data[0].items[i].volumeInfo.imageLinks.thumbnail;
+aEL.appendChild(imgEL);
+cardsEl.appendChild(aEL);
+
+searchResEl.appendChild(cardsEl);
+
+
+
+  }
+
+
+}
+
+
 
 function BooksbyAuthor(data)
 {
@@ -137,11 +144,94 @@ function BooksbyAuthor(data)
   
   searchResEl.appendChild(cardsEl);
   
- }
+  
+  
+  }
 
   searchResEl.appendChild(colEl);
 
 } 
+
+function BooksbyTitle(data)
+{
+
+  searchResEl.innerHTML="";
+
+  var colEl=document.createElement("div");
+    colEl.className="col-md-3 card-group";
+    
+
+  for(i=0;i<data[0].docs.length;i++)
+  {
+  
+  var cardsEl=document.createElement("div");
+  cardsEl.className="card ws";
+ 
+  colEl.appendChild(cardsEl);
+
+  var imgEL=document.createElement("img");
+  imgEL.className="img-thumbnail";
+  imgEL.src="https://covers.openlibrary.org/b/id/"+data[0].docs[i].cover_i+"-L.jpg";
+  cardsEl.appendChild(imgEL);  
+  
+  var cardbodyEl=document.createElement("div");
+  cardbodyEl.className="card-body";
+  cardsEl.appendChild(cardbodyEl);
+  
+  var h5El=document.createElement("h5");
+  h5El.className="card-title";
+  h5El.innerHTML=data[0].docs[i].title.toUpperCase();
+  cardbodyEl.appendChild(h5El);
+  
+  var pEl=document.createElement("p");
+  pEl.className="card-text";
+  //pEl.innerHTML=" "+data[1].items[i].volumeInfo.description;
+  cardbodyEl.appendChild(pEl);
+
+  var ulEl=document.createElement("ul");
+  ulEl.className="list-group list-group-flush";
+  cardsEl.appendChild(ulEl);
+
+  var liEL=document.createElement("li");
+  liEL.className="list-group-item";
+  
+  liEL.innerHTML="ISBN: "+data[0].docs[i].isbn[0];
+  //if(data[0].docs[i].birth_date==undefined){liEL.innerHTML="Birth Date:";}
+ 
+  ulEl.appendChild(liEL);
+
+  liEL=document.createElement("li");
+  liEL.className="list-group-item";
+  liEL.innerHTML="Publisher: "+data[0].docs[i].publisher[0];
+  ulEl.appendChild(liEL);
+
+  liEL=document.createElement("li");
+  liEL.className="list-group-item";
+  liEL.innerHTML="Author(s): "+data[0].docs[i].author_name;
+  ulEl.appendChild(liEL);
+
+  cardbodyEl=document.createElement("div");
+  cardbodyEl.className="card-body";
+  var aEL=document.createElement("a");
+  aEL.href="https://openlibrary.org/"+data[0].docs[i].key;
+  aEL.className="card-link";
+  aEL.innerHTML="Details";
+
+  cardsEl.appendChild(cardbodyEl);
+
+  cardsEl.appendChild(aEL);
+  
+  searchResEl.appendChild(cardsEl);
+  
+  searchResEl.appendChild(colEl);
+  
+  }
+}
+
+
+
+
+
 
 function NYTBest() 
 {
@@ -161,8 +251,10 @@ NYTEl.innerHTML="";
         imgEL.className="card-img-top";
         imgEL.src=data.results.books[i].book_image;
         
+        
         aEL.appendChild(imgEL);
         nycardEl.appendChild(aEL);
+        
         
         var nycardfooterEl=document.createElement("div");
         nycardfooterEl.className="card-footer";
@@ -174,10 +266,17 @@ NYTEl.innerHTML="";
         
         }
           
-      });
+      
+      
+
+          
+    });
   });
   
+
+
 }
+
 
 var getApi= function(event) {
   event.preventDefault();
@@ -190,33 +289,12 @@ console.log(radioValue);
 if(radioValue=="title")
 {
   requestUrl1="http://openlibrary.org/search.json?title="+searchword; 
-  requestUrl2="https://www.googleapis.com/books/v1/volumes?q="+searchword+"intitle&key="+GBooksApiID;
-}// to search title
-  else if(radioValue=="subject")
-  {
-requestUrl1="http://openlibrary.org/subjects/"+searchword+".json"; // to search genre or subject
+  //requestUrl2 ="https://www.googleapis.com/books/v1/volumes?q=\""+searchword+"\"+intitle&key="+GBooksApiID;
+  console.log(requestUrl1);
 
-  }
-else if(radioValue=="author")
-{
-  requestUrl1="https://openlibrary.org/search/authors.json?q="+searchword;
-
-}
-
-  else 
-  {
-    requestUrl1="https://openlibrary.org/"+radioValue+"/"+searchword+".json"; // to search authors and isbn#
-    requestUrl2="https://www.googleapis.com/books/v1/volumes?q="+searchword+"isbn&key="+GBooksApiID;
-  }
-
- console.log(requestUrl1);
-
- var requestUrl2 ="https://www.googleapis.com/books/v1/volumes?q="+searchword+"isbn&key="+GBooksApiID;
-
-
-Promise.all([
-    fetch(requestUrl1),
-    fetch(requestUrl2),
+  Promise.all([
+    fetch(requestUrl1)
+    //fetch(requestUrl2)
   ]).then(function (responses) {
     // Get a JSON object from each of the responses
     return Promise.all(responses.map(function (response) {
@@ -226,21 +304,109 @@ Promise.all([
     // Log the data to the console
     // You would do something with both sets of data here
     console.log(data);
-    data1=data;
+  
+    BooksbyTitle(data);
    
-    //BooksbyISBN2(data);
-    BooksbyAuthor(data);
 
   }).catch(function (error) {
     // if there's an error, log it
     console.log(error);
   });
-  if(radioValue== "isbn"){
-    BooksbyISBN(data1);  
+
+
+
+}// to search title
+  else if(radioValue=="subject")
+  {
+//requestUrl1="http://openlibrary.org/subjects/"+searchword+".json"; // to search genre or subject
+requestUrl2 ="https://www.googleapis.com/books/v1/volumes?q="+searchword+"+subject&maxResults=40&key="+GBooksApiID;
+
+Promise.all([
+  
+  fetch(requestUrl2)
+]).then(function (responses) {
+  // Get a JSON object from each of the responses
+  return Promise.all(responses.map(function (response) {
+    return response.json();
+  }));
+}).then(function (data) {
+  // Log the data to the console
+  // You would do something with both sets of data here
+  console.log(data);
+
+  BooksbySubject(data);
+ 
+
+}).catch(function (error) {
+  // if there's an error, log it
+  console.log(error);
+});
+
+
   }
-  if(radioValue== "author"){
-    BooksbyAuthor(data1);  
-  }
-  }
+else if(radioValue=="author")
+{
+  requestUrl1="https://openlibrary.org/search/authors.json?q="+searchword;
+  requestUrl2 ="https://www.googleapis.com/books/v1/volumes?q="+searchword+"+inauthor&key="+GBooksApiID;
+
+  Promise.all([
+    fetch(requestUrl1),
+    //fetch(requestUrl2)
+  ]).then(function (responses) {
+    // Get a JSON object from each of the responses
+    return Promise.all(responses.map(function (response) {
+      return response.json();
+    }));
+  }).then(function (data) {
+    // Log the data to the console
+    // You would do something with both sets of data here
+    console.log(data);
+  
+    BooksbyAuthor(data);
+   
+
+  }).catch(function (error) {
+    // if there's an error, log it
+    console.log(error);
+  });
+
+
+}
+
+  else //by ISBN
+  {
+    //requestUrl1="https://openlibrary.org/"+radioValue+"/"+searchword+".json"; // to search authors and isbn#
+    requestUrl2 ="https://www.googleapis.com/books/v1/volumes?q="+searchword+"+isbn&maxResults=10&key="+GBooksApiID;
+
+
+    Promise.all([
+      fetch(requestUrl2)
+    ]).then(function (responses) {
+      // Get a JSON object from each of the responses
+      return Promise.all(responses.map(function (response) {
+        return response.json();
+      }));
+    }).then(function (data) {
+      // Log the data to the console
+      // You would do something with both sets of data here
+      console.log(data);
+    
+      BooksbyISBN(data);
+     
+  
+    }).catch(function (error) {
+      // if there's an error, log it
+      console.log(error);
+    });
+  
+
+  }    
+} 
+
+
+
+
+
 NYTBest();
+
 userFormEl.addEventListener("submit", getApi);
